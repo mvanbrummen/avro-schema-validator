c2e160cd27eb79b8f82c48c511c3279a468e5804
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-03-11 15:38:31 +1000

    Added working example

--- src/main/resources/static/src/App.js
+++ src/main/resources/static/src/App.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      schemaValue: null,
+      jsonValue: null,
+      showValidationMessage: false,
+      validationMessage: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+          [target.name]: target.value
+        });
+  }
+
+  validate() {
+
+  let req = JSON.stringify({
+    schema: this.state.schemaValue,
+    json: this.state.jsonValue
+  });
+
+  console.log(req);
+
+    fetch('http://localhost:8080/validate', {
+    method: 'POST',
+     mode: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: req
+    })
+    .then(res => res.json())
+    .then(data => {
+         this.setState({
+            showValidationMessage: true,
+            validationMessage: data.valid ? "JSON validated against Avro Schema" ? "Invalid: " + data.message
+        });
+    })
+    .catch(e => {
+        console.log('Error:' + e);
+    })
+  }
+
+  clear() {
+         document.getElementById("validationForm").reset();
+              this.setState({
+                         showValidationMessage: false
+                     });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+      <form id="validationForm">
+        <textarea
+            placeholder="Enter an Avro schema..."
+            name="schemaValue"
+            onChange={this.handleChange}
+        ></textarea>
+        <textarea
+            placeholder="Enter some JSON..."
+            name="jsonValue"
+             onChange={this.handleChange}
+            >
+        </textarea>
+
+        { this.state.showValidationMessage &&
+          <h3>{ this.state.validationMessage }</h3>
+        }
+
+        <input type="button" value="Clear" onClick={this.clear.bind(this)}/>
+        <input className="validate-submit-btn" type="button" value="Validate" onClick={this.validate.bind(this)}/>
+
+</form>
       </div>
     );
   }
