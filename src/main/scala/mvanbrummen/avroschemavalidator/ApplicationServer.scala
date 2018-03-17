47c72e8bb24029e46254bb50b0ddc0c75412f673
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-03-17 10:36:14 +1000

    Added config

--- src/main/scala/mvanbrummen/avroschemavalidator/ApplicationServer.scala
+++ src/main/scala/mvanbrummen/avroschemavalidator/ApplicationServer.scala
@@ -1,12 +1,13 @@
 package mvanbrummen.avroschemavalidator
 
-import akka.actor.{ ActorRef, ActorSystem }
+import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import mvanbrummen.avroschemavalidator.actors.AvroSchemaValidatorActor
-import mvanbrummen.avroschemavalidator.routes.{ AvroSchemaValidationRoutes, StaticRoutes }
+import mvanbrummen.avroschemavalidator.routes.{AvroSchemaValidationRoutes, StaticRoutes}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -16,11 +17,13 @@ object ApplicationServer extends App with AvroSchemaValidationRoutes with Static
   implicit val system: ActorSystem = ActorSystem("avroSchemaValidatorAkkaHttpServer")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
+  val config = ConfigFactory.load()
+
   val avroSchemaValidationActor: ActorRef = system.actorOf(AvroSchemaValidatorActor.props, "avroSchemaValidatorActor")
 
   lazy val routes: Route = concat(staticResourcesRoutes, avroSchemaValidationRoutes)
 
-  Http().bindAndHandle(routes, "localhost", 8080)
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
 
   println(s"Server online at http://localhost:8080/")
 
